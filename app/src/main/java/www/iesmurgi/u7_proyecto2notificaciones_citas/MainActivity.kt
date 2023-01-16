1358dec807019b4908df1ddc43fc99e6a4182d9f
DANIEL2DAM <dmarrom577@g.educaand.es>
2023-01-16 11:06:04 +0100

    Initial commit

---
+++ app/src/main/java/www/iesmurgi/u7_proyecto2notificaciones_citas/MainActivity.kt
@@ -0,0 +1,11 @@
+package www.iesmurgi.u7_proyecto2notificaciones_citas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
